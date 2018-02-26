189ec822d9e9a3d8a6655b40592fd7261ee2b68f
Paul Kristoff <>
2018-02-26 12:50:10 -0500

    Styling code cleanup

--- R/hello.R
+++ R/hello.R
@@ -17,12 +17,12 @@ updateRadio <-
 
 mmUI <- function() {
   generateRadioButton <- function(id, label) {
-    htmlRed <- "<p style='color:red;'>Red</p>"
-    htmlGreen <- "<p style='color:green;'>Green</p>"
-    htmlBlue <- "<p style='color:blue;'>Blue</p>"
-    htmlOrange <- "<p style='color:orange;'>Orange</p>"
-    htmlBlack <- "<p style='color:black;'>Black</p>"
-    htmlWhite <- "<p style='color:white;'>White</p>"
+    htmlRed <- "<p style='color:red;font-size:10px;'>Red</p>"
+    htmlGreen <- "<p style='color:green;font-size:10px;'>Green</p>"
+    htmlBlue <- "<p style='color:blue;font-size:10px;'>Blue</p>"
+    htmlOrange <- "<p style='color:orange;font-size:10px;'>Orange</p>"
+    htmlBlack <- "<p style='color:black;font-size:10px;'>Black</p>"
+    htmlWhite <- "<p style='color:white;font-size:10px;'>White</p>"
 
     column(
       3,
@@ -45,49 +45,95 @@ mmUI <- function() {
   pageWithSidebar(
     headerPanel("Master Mind"),
 
-    sidebarPanel(shinyjs::useShinyjs(),
-                 column(
-                   12,
-                   fluidRow(
-                     generateRadioButton("cell1", "Cell 1"),
-                     generateRadioButton("cell2", "Cell 2"),
-                     generateRadioButton("cell3", "Cell 3"),
-                     generateRadioButton("cell4", "Cell 4")
-                   )
-                 )),
+    sidebarPanel(
+      shinyjs::useShinyjs(),
+      fluidRow(style = "background-color:aqua;",
+        generateRadioButton("cell1", "Cell 1"),
+        generateRadioButton("cell2", "Cell 2"),
+        generateRadioButton("cell3", "Cell 3"),
+        generateRadioButton("cell4", "Cell 4")
+      )
+    ),
 
     mainPanel(fixedRow(
       column(
         12,
         #style = "background-color:pink;",
         fixedRow(
+          column(3, 'Current Guess'),
           column(1, style = "background-color:aqua;", htmlOutput('txtcell1')),
           column(1, style = "background-color:aqua;", htmlOutput('txtcell2')),
           column(1, style = "background-color:aqua;", htmlOutput('txtcell3')),
           column(1, style = "background-color:aqua;", htmlOutput('txtcell4'))
         ),
         fixedRow(
-          column(6, style = "background-color:pink;", dataTableOutput('boardGuess')),
-          column(6, style = "background-color:pink;", dataTableOutput('boardResult'))
+          column(12, style = "background-color:pink;", dataTableOutput('board'))
         ),
         fixedRow(column(
-          3, style = "background-color:aqua;", actionButton("showResults", "Show Results")
+          2, actionButton("showResults", "Show Result")
         ))
       )
     ))
   )
 }
+updateResults <- function (input, code) {
+  nextPos <- 6
+  result <- c(input$cell1,
+              input$cell2,
+              input$cell3,
+              input$cell4,
+              '',
+              '',
+              '',
+              '',
+              '')
+  guess <- c(input$cell1,
+             input$cell2,
+             input$cell3,
+             input$cell4)
+  # print(paste('code:', code))
+  # print(paste('guess:', guess))
+  posFound <- c()
+  posNotFound <- c()
+  for (pos in 1:4) {
+    if (guess[pos] == code[pos]) {
+      posFound <- append(posFound, pos)
+      result[nextPos] <- 'black'
+      nextPos <- nextPos + 1
+    } else {
+      posNotFound <- append(posNotFound, pos)
+    }
+  }
+  # print(paste('posFound:', posFound))
+  # print(paste('posNotFound:', posNotFound))
+  for (posNF in posNotFound) {
+    color = guess[posNF]
+    # print(paste('looking for color:',color))
+    for (pos2 in 1:4) {
+      if (match(pos2, posFound, nomatch = 0) == 0) {
+        # print(paste('code:',code))
+        # print(paste('pos2:',pos2,'code[pos2]:',code[pos2]))
+        # print(paste('pos2:', pos2, 'code[pos2]:', code[pos2], 'color:', color))
+        if (color == code[pos2]) {
+          # print(paste('found color:',color))
+          posFound <- append(posFound, pos2)
+          result[nextPos] <- 'white'
+          nextPos <- nextPos + 1
+        } else{
+          # print(paste('code[pos2]:',code[pos2]))
+        }
+      }
+    }
+  }
+  result
+}
 server <- function(input, output, session) {
   code = sample(c('black', 'blue', 'green', 'orange', 'red', 'white'), 4)
   print(code)
-  localBoardGuess <- matrix('',
-                            ncol = 4,
-                            nrow = 10,
-                            byrow = TRUE)
-  localBoardResult <- matrix('',
-                             ncol = 4,
-                             nrow = 10,
-                             byrow = TRUE)
+  localBoard <- matrix('',
+                       ncol = 9,
+                       nrow = 10,
+                       byrow = TRUE)
   currentRowIndex <- 1
   # Update current row
   output$txtcell1 <- renderText({
@@ -109,52 +155,9 @@ server <- function(input, output, session) {
   #' @examples
   #'
   observeEvent(input$showResults, {
-    nextPos <- 1
-    result <- c('',
-                '',
-                '',
-                '')
-    guess <- c(input$cell1,
-               input$cell2,
-               input$cell3,
-               input$cell4)
-    # print(paste('code:', code))
-    # print(paste('guess:', guess))
-    posFound <- c()
-    posNotFound <- c()
-    for (pos in 1:4) {
-      if (guess[pos] == code[pos]) {
-        posFound <- append(posFound, pos)
-        result[nextPos] <- 'black'
-        nextPos <- nextPos + 1
-      } else {
-        posNotFound <- append(posNotFound, pos)
-      }
-    }
-    # print(paste('posFound:', posFound))
-    # print(paste('posNotFound:', posNotFound))
-    for (posNF in posNotFound) {
-      color = guess[posNF]
-      # print(paste('looking for color:',color))
-      for (pos2 in 1:4) {
-        if (match(pos2, posFound, nomatch = 0) == 0) {
-          # print(paste('code:',code))
-          # print(paste('pos2:',pos2,'code[pos2]:',code[pos2]))
-          # print(paste('pos2:', pos2, 'code[pos2]:', code[pos2], 'color:', color))
-          if (color == code[pos2]) {
-            # print(paste('found color:',color))
-            posFound <- append(posFound, pos2)
-            result[nextPos] <- 'white'
-            nextPos <- nextPos + 1
-          } else{
-            # print(paste('code[pos2]:',code[pos2]))
-          }
-        }
-      }
-    }
-    localBoardGuess[currentRowIndex, ] <<- guess
-    localBoardResult[currentRowIndex, ] <<- result
-    # printlocalBoardGuess()
+
+    localBoard[currentRowIndex,] <<- updateResults(input, code)
+    printLocalBoard()
     outputBoard()
     currentRowIndex <<- currentRowIndex + 1
     updateRadio(session, 'cell1', label = 'Cell 1', value = 'black')
@@ -162,8 +165,8 @@ server <- function(input, output, session) {
     updateRadio(session, 'cell3', label = 'Cell 3', value = 'black')
     updateRadio(session, 'cell4', label = 'Cell 4', value = 'black')
   })
-  printlocalBoardGuess <- function() {
-    print(localBoardGuess[currentRowIndex,])
+  printLocalBoard <- function() {
+    print(localBoard[currentRowIndex, ])
   }
   outputBoard <- function() {
     values = matrix(
@@ -172,18 +175,20 @@ server <- function(input, output, session) {
       nrow = 10,
       byrow = TRUE
     )
-    columnNames <- c('1', '2', '3', '4')
+    columnNames <- c('1', '2', '3', '4', '', '1', '2', '3', '4')
 
-    datatableGuess <- renderDataTable({
+    xxx <- renderDataTable({
       dat <-
         datatable(
-          localBoardGuess,
+          localBoard,
           colnames = columnNames,
           container = withTags(table(class = 'display',
                                      thead(
-                                       tr(th(
-                                         colspan = 4, style = "text-align:center", 'Guess'
-                                       )),
+                                       tr(
+                                         th(colspan = 4, style = "text-align:center", 'Guess'),
+                                         th(colspan = 1, style = "text-align:center", ''),
+                                         th(colspan = 4, style = "text-align:center", 'Result')
+                                       ),
                                        tr(lapply(paste(columnNames), th))
                                      ))),
           options = list(
@@ -203,39 +208,8 @@ server <- function(input, output, session) {
         )
       return(dat)
     })
-    output$boardGuess <- datatableGuess
-
-    datatableResult <- renderDataTable({
-      dat <-
-        datatable(
-          localBoardResult,
-          colnames = columnNames,
-          container = withTags(table(class = 'display',
-                                     thead(
-                                       tr(th(
-                                         colspan = 4, style = "text-align:center", 'Result'
-                                       )),
-                                       tr(lapply(paste(columnNames), th))
-                                     ))),
-          options = list(
-            paging = FALSE,
-            searching = FALSE,
-            ordering = FALSE,
-            dom = 't'
-          )
-        ) %>%
-        formatStyle(
-          columns = 1:4,
-          valueColumns = 1:4,
-          color = styleEqual(
-            levels = c('black', 'blue', 'green', 'orange', 'red', 'white'),
-            values = c('black', 'blue', 'green', 'orange', 'red', 'white')
-          )
-        )
-      return(dat)
-    })
-    # print(datatableGuess)
-    output$boardResult <- datatableResult
+    # print(xxx)
+    output$board <- xxx
   }
 }
 
